671ea89883c2d56ebe2d6ba7151a823d5a007d59
Alain Tresor <tresoralain35@gmail.com>
2022-09-14 13:57:14 +0200

    testing heroku deployment

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('/', function () {
-        return response()->json(['message' => 'Welcome to API']);
+        return response()->json(['message' => 'Welcome to APIs']);
     });
     // ------------------Admin routes----------------------
     Route::prefix('Auth')->group(function () {
